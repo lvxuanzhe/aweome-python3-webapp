6ec2f1123b781254f5d939d5e00f65d9cc44e3c9
lvxuanzhe <lvxuanzhe@nuaa.edu.cn>
2017-08-05 19:48:19 +0800

    加入logging.INFO

--- www/model_test.py
+++ www/model_test.py
@@ -2,14 +2,15 @@
 # _*_ coding:utf-8 _*_
 # Author:Xuanzhe Lv
 
+import logging;logging.basicConfig(level=logging.INFO)
 import orm
 from model import User,Blog,Comment
 import asyncio
 
 async def test(loop):
     await orm.create_pool(user='www-data',password='www-data',db='awesome',loop=loop)
-    u = User(name='Test',email='test@example.com',passwd='12345678',image='about:blank')
+    u = User(name='Test2',email='test2@example.com',passwd='12345678',image='about:blank')
     await u.save()
-
+    
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
